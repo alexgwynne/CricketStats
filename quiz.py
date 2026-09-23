d45361d6b73f5b4cc345741b9f1c8e886b539d2a
alexgwynne <alex.gwynne2974@gmail.com>
2026-09-23 22:32:02 +1200

    First Commit of the actual project is the 'tkinter' and 'root' code. This is regarding the graphical interface that the quiz will appear on. The first test of this code didn't cause a window to appear, this is discussed in the testing log.

--- quiz.py
+++ quiz.py
@@ -1,2 +1,19 @@
-#test
+#Cricket Rules Quiz with graphical interface
+#By Alex Gwynne
+#Comments are above what they comment on
 
+#This 'tkinter' code is what allows the python program to appear in a graphical interface rather than the terminal
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+quizquestions = [
+
+]
+
+#'root' code is for the main window that the graphical interface appears in
+root = tk.Tk()
+root.title("Cricket Rules Quiz")
+root.geometry("500x400")
+
+#Required at the end of the python program for it to work
+root.mainloop()
